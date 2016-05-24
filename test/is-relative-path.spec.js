d7bd8eef6b5aabfd50d99ebafd84f0d06fd4d001
RoCky Wu <rwu@cepave.com>
2016-05-24 21:25:55 +0800

    Add test spec

--- test/is-relative-path.spec.js
+++ test/is-relative-path.spec.js
@@ -5,18 +5,18 @@ test('should be not a relative require path', assert => {
   assert.is(isRelativePath('README.md'), false)
 })
 
-
 test('should be a relative require path: ./', assert => {
   assert.is(isRelativePath('./README.md'), true)
 })
-
 
 test('should be a relative require path: .../', assert => {
   assert.is(isRelativePath('.../README.md'), true)
 })
 
-
 test('should be not a relative require path: /', assert => {
   assert.is(isRelativePath('/README.md'), false)
 })
 
+test('should be a relative require path: ../', assert => {
+  assert.is(isRelativePath('../'), true)
+})
